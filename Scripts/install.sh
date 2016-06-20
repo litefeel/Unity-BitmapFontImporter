a5f61988c6bca764c87fec9a0eeeca08e722307d
lite3 <lite3@qq.com>
2016-06-20 12:36:00 +0800

    update ci

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -2,8 +2,17 @@
 # exit this script if any commmand fails
 set -e
 
-echo "Downloading from $UNITY_DOWNLOAD_URL: "
-curl -o Unity.pkg "$UNITY_DOWNLOAD_URL"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "Get Unity version for $UNITY_VERSION"
+url=$(python $DIR/unitydownloadurl.py -v $UNITY_VERSION --os mac)
+realVersion=${url/*Unity-/}
+realVersion=${realVersion/.pkg*/}
+echo "Got Unity version:$realVersion"
+echo "Got Unity url:$url"
+
+echo "Downloading from $url: "
+curl -o Unity.pkg "$url"
 
 echo 'Installing Unity.pkg'
 sudo installer -dumplog -package Unity.pkg -target /
