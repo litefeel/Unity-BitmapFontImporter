51efee80a52b54b9719a37fe5ee017f542154883
zheyu <lite3@qq.com>
2017-02-10 19:08:53 +0800

    fix new font can not display via Text in unity 5.5

--- Assets/BitmapFontImporter/Editor/BFImporter.cs
+++ Assets/BitmapFontImporter/Editor/BFImporter.cs
@@ -84,13 +84,17 @@ namespace litefeel
             font.characterInfo = parse.charInfos;
             
             AssetDatabase.SaveAssets(); 
+
+#if UNITY_5_5
+            // unity 5.5 can not load custom font
+            ReloadFont(fontPath);
+#endif
         }
 
         private static void UpdateKernings(SerializedObject so, Kerning[] kernings)
         {
             int len = kernings != null ? kernings.Length : 0;
             SerializedProperty kerningsProp = so.FindProperty("m_KerningValues");
-            //so.Update();
 
             if (len == 0)
             {
@@ -135,6 +139,30 @@ namespace litefeel
             string oldFontPath = oldFntPath.Substring(0, oldFntPath.Length - 4) + ".fontsettings";
             string nowFontPath = nowFntPath.Substring(0, nowFntPath.Length - 4) + ".fontsettings";
             AssetDatabase.MoveAsset(oldFontPath, nowFontPath);
+        }
+
+        // new font can not display via Text in unity 5.5
+        // must import import it
+        private static void ReloadFont(string fontPath)
+        {
+            var tmpPath = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            AssetDatabase.ExportPackage(fontPath, tmpPath);
+            AssetDatabase.DeleteAsset(fontPath);
+            
+            var startTime = DateTime.Now;
+            EditorApplication.CallbackFunction func = null;
+            func = () =>
+            {
+                TimeSpan dalt = DateTime.Now - startTime;
+                if (dalt.TotalSeconds >= 0.1)
+                {
+                    EditorApplication.update -= func;
+                    AssetDatabase.ImportPackage(tmpPath, false);
+                    File.Delete(tmpPath);
+                }
+            };
+
+            EditorApplication.update += func;
         }
     }
 
