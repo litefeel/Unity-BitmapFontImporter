fbd2663969b1de9b652ce52c9aac9889fe7cd281
zheyu <lite3@qq.com>
2017-02-13 12:51:08 +0800

    fixed fnt parse error when letter="""

--- Assets/BitmapFontImporter/Editor/FntParse.cs
+++ Assets/BitmapFontImporter/Editor/FntParse.cs
@@ -107,7 +107,12 @@ namespace litefeel
         private Regex pattern;
         public void DoTextParse(ref string content)
         {
-            pattern = new Regex(@"\S+="".*?""|\S+");
+            // letter=" "       // \S+=".+?"
+            // letter="x"       // \S+=".+?"
+            // letter="""       // \S+=".+?"
+            // letter=""        // \S+
+            // char             // \S+
+            pattern = new Regex(@"\S+="".+?""|\S+");
             string[] lines = content.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
             ReadTextInfo(ref lines[0]);
             ReadTextCommon(ref lines[1]);
