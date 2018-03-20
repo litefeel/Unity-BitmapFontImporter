c0266fc7b4a93dcc49f2381ff7ec052b80724779
litefeel <litefeel@gmail.com>
2018-03-20 17:52:48 +0800

    update test

--- Assets/Test/Editor/MyTest.cs
+++ Assets/Test/Editor/MyTest.cs
@@ -9,8 +9,10 @@ public class MyTest {
     public static void Test () {
         AssetDatabase.DeleteAsset("Assets/Plugins/BitmapFontImporter/Examples/Font1/font1.fontsettings");
         AssetDatabase.DeleteAsset("Assets/Plugins/BitmapFontImporter/Examples/Font2/font2.fontsettings");
+        AssetDatabase.DeleteAsset("Assets/Plugins/BitmapFontImporter/Examples/Font2/font3.fontsettings");
 
         BFImporter.DoImportBitmapFont("Assets/Plugins/BitmapFontImporter/Examples/Font1/font1.fnt");
         BFImporter.DoImportBitmapFont("Assets/Plugins/BitmapFontImporter/Examples/Font2/font2.fnt");
+        BFImporter.DoImportBitmapFont("Assets/Plugins/BitmapFontImporter/Examples/Font3/font2.fnt");
     }
 }
