78bd0eb24834356b74cf691af1284883ff44b859
litefeel <litefeel@gmail.com>
2018-03-20 17:36:17 +0800

    correct test

--- Assets/Test/Editor/MyTest.cs
+++ Assets/Test/Editor/MyTest.cs
@@ -7,10 +7,10 @@ public class MyTest {
 
     //[InitializeOnLoadMethod]
     public static void Test () {
-        AssetDatabase.DeleteAsset("Assets/BitmapFontImporter/Examples/Font1/font1.fontsettings");
-        AssetDatabase.DeleteAsset("Assets/BitmapFontImporter/Examples/Font2/font2.fontsettings");
+        AssetDatabase.DeleteAsset("Assets/Plugins/BitmapFontImporter/Examples/Font1/font1.fontsettings");
+        AssetDatabase.DeleteAsset("Assets/Plugins/BitmapFontImporter/Examples/Font2/font2.fontsettings");
 
-        BFImporter.DoImportBitmapFont("Assets/BitmapFontImporter/Examples/Font1/font1.fnt");
-        BFImporter.DoImportBitmapFont("Assets/BitmapFontImporter/Examples/Font2/font2.fnt");
+        BFImporter.DoImportBitmapFont("Assets/Plugins/BitmapFontImporter/Examples/Font1/font1.fnt");
+        BFImporter.DoImportBitmapFont("Assets/Plugins/BitmapFontImporter/Examples/Font2/font2.fnt");
     }
 }
